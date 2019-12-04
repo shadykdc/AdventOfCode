9c0bf8644dfaa2c2d0dae26d5ed5a019210ef350
Katie Shea <kathryn.shea@gmail.com>
2019-12-03 21:25:57 -0800

    ready for day 5

---
+++ aoc5.py
@@ -0,0 +1,10 @@
+# Advent of Code Day 5
+
+# Parse Input
+with open('input3.txt') as f:
+    lines = f.readlines()
+
+list1 = lines[0].strip().split(",") 
+list2 = lines[1].strip().split(",") 
+
+print(list1)
