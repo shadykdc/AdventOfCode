71bb8d16bcb445f67f39e2383fb2447a5d7a1445
Katie Shea <katieshea@fb.com>
2020-12-06 12:29:54 +0000

    add empty day 8

---
+++ 2020/8.cpp
@@ -0,0 +1,32 @@
+/*
+    Advent of Code Day 8
+
+    To run on a mac:
+
+        1. Update makefile "DAY" variable to 8
+        2. Run "make"
+        3. Run "./aoc"
+
+    Problem:
+
+        https://adventofcode.com/2020/day/8
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#define INPUT_FILE "input8.txt"
+
+int main(int argc, char *argv[])
+{
+    // read file into nums
+    ifstream ifs(INPUT_FILE);
+
+    cout << "Day 8" << endl;
+
+    return 0;
+}
