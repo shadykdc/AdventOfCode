8df2dde519e35048b3fef7da887bb4172f90ad32
Katie Shea <katieshea@fb.com>
2020-12-13 05:21:04 +0000

    part 1

--- 2020/13.cpp
+++ 2020/13.cpp
@@ -15,20 +15,77 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 #define INPUT_FILE "input13.txt"
 
-void read_file()
+int read_file(vector<int>& bus_ids)
 {
-    return;
+    string str;
+    int times;
+    ifstream ifs(INPUT_FILE, ifstream::in);
+
+    // read in the map of trees to grid
+    if (ifs.good())
+    {
+        getline(ifs, str);
+        times = stoi(str);
+    }
+    while(ifs.good())
+    {
+        getline(ifs, str);
+        std::stringstream ss(str);
+        for (int i; ss >> i;)
+        {
+            bus_ids.push_back(i);
+            while (ss.peek() == ',' || ss.peek() == 'x')
+            {
+                if (ss.peek() == 'x')
+                    bus_ids.push_back(-1);
+                ss.ignore();
+            }
+        }
+    }
+    ifs.close();
+    return times;
+}
+
+int part1(vector<int>& bus_ids, int time)
+{
+    int wait = INT_MAX;
+    int bus_id = 0;
+
+    for (int num : bus_ids)
+    {
+        if (num == -1) continue;
+        for (int i = 0; i < num; i++)
+        {
+            int depart = time + i;
+            if (depart%num == 0 && i < wait)
+            {
+                wait = i;
+                bus_id = num;
+            }
+        }
+    }
+
+    return wait * bus_id;
+}
+
+int part2(vector<int>& bus_ids)
+{
+    return 1;
 }
 
 int main(int argc, char *argv[])
 {
+    vector<int> bus_ids;
+    int time = read_file(bus_ids);
 
-    read_file();
+    cout << part1(bus_ids, time) << endl; // 3606
+    cout << part2(bus_ids) << endl;
 
     return 0;
 }
