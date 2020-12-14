2054d896ab67ec5e271ec658b845778d19438263
Katie Shea <katieshea@fb.com>
2020-12-14 20:15:30 +0000

    day 15

---
+++ 2020/16.cpp
@@ -0,0 +1,34 @@
+/*
+    Advent of Code Day 16
+
+    To run on a mac:
+
+        1. Update makefile "DAY" variable to 16
+        2. Run "make"
+        3. Run "./aoc"
+
+    Problem:
+
+        https://adventofcode.com/2020/day/16
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#define INPUT_FILE "input16.txt"
+
+void read_file()
+{
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+
+    read_file();
+
+    return 0;
+}
