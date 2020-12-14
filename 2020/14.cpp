b00014070c8dc10c5401b585581a191fc53d6f8b
Katie Shea <katieshea@fb.com>
2020-12-14 10:28:20 +0000

    part 1

--- 2020/14.cpp
+++ 2020/14.cpp
@@ -15,20 +15,94 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bitset>
+#include <string>
+#include <stdio.h>
 
 using namespace std;
 
 #define INPUT_FILE "input14.txt"
 
-void read_file()
+unsigned long part1()
 {
-    return;
+    vector<bitset<36>> memory;
+    int idx;
+    string str;
+    string mask;
+    ifstream ifs(INPUT_FILE, ifstream::in);
+    int offset = strlen("mask = ");
+
+    if (ifs.good()) getline(ifs, mask);
+    if (mask.size() > offset) mask = mask.substr(7);
+
+    while (ifs.good())
+    {
+        getline(ifs, str);
+        if(sscanf(&str[0], "mem[%d] = %s ", &idx, &str[0]) == 2)
+        {
+            bitset<36> val (stoi(str));
+            if (idx >= memory.size()) memory.resize(idx+1);
+            for (int i = 0; i < mask.size(); i++)
+            {
+                if (mask[i] != 'X')
+                    // the index of the bitset is "backwards"
+                    val.set(val.size() - 1 - i, mask[i] - '0');
+            }
+            memory[idx] = val;
+        }
+        else if (str.size() > offset)
+            mask = str.substr(offset);
+    }
+    ifs.close();
+
+    unsigned long sum = 0;
+    for (auto val : memory)
+        sum += val.to_ulong();
+
+    return sum;
+}
+
+unsigned long part2()
+{
+    vector<bitset<36>> memory;
+    int idx;
+    string str;
+    string mask;
+    ifstream ifs(INPUT_FILE, ifstream::in);
+    int offset = strlen("mask = ");
+
+    if (ifs.good()) getline(ifs, mask);
+    if (mask.size() > offset) mask = mask.substr(7);
+
+    while (ifs.good())
+    {
+        getline(ifs, str);
+        if(sscanf(&str[0], "mem[%d] = %s ", &idx, &str[0]) == 2)
+        {
+            bitset<36> val (stoi(str));
+            if (idx >= memory.size()) memory.resize(idx+1);
+            for (int i = 0; i < mask.size(); i++)
+            {
+                // do something different
+            }
+            memory[idx] = val;
+        }
+        else if (str.size() > offset)
+            mask = str.substr(offset);
+    }
+    ifs.close();
+
+    unsigned long sum = 0;
+    for (auto val : memory)
+        sum += val.to_ulong();
+
+    return sum;
 }
 
 int main(int argc, char *argv[])
 {
-
-    read_file();
+    cout << part1() << endl; // 17481577045893
+    cout << part1() << endl; //
 
     return 0;
 }
