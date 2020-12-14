979066c53f438c98a0fed183a0118d8a47373a4b
Katie Shea <katieshea@fb.com>
2020-12-14 20:05:41 +0000

    not proud of this one, ha

--- 2020/14.cpp
+++ 2020/14.cpp
@@ -14,10 +14,11 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <unordered_map>
 #include <bitset>
 #include <string>
 #include <stdio.h>
+#include <vector>
 
 using namespace std;
 
@@ -62,9 +63,50 @@ unsigned long part1()
     return sum;
 }
 
+void comb(int count_x, vector<bitset<36>>& addresses, bitset<36>& mem_addr, string mask)
+{
+    if (count_x == 0) addresses.emplace_back(mem_addr);
+
+    for (int i = 0; i < mask.size(); i++)
+    {
+        if (mask[i] == 'X')
+        {
+            mask[i] = 'Y';
+
+            mem_addr.set(mem_addr.size()-1-i, 1);
+            comb(count_x-1, addresses, mem_addr, mask);
+
+            mem_addr.set(mem_addr.size()-1-i, 0);
+            comb(count_x-1, addresses, mem_addr, mask);
+
+            break;
+        }
+    }
+}
+
+void populate_mem(const string str, const int idx, const string mask,
+                  unordered_map<unsigned long, bitset<36>>& memory)
+{
+    bitset<36> val(stoi(str));
+    bitset<36> mem_addr(idx);
+    vector<bitset<36>> addresses;
+    int count_x = 0;
+    for (int i = 0; i < mask.size(); i++)
+    {
+        size_t mem_addr_i = mem_addr.size() - 1 - i;
+        if (mask[i] == '1')  mem_addr.set(mem_addr_i, 1);
+        else if (mask[i] == 'X') count_x++;
+    }
+
+    comb(count_x, addresses, mem_addr, mask);
+
+    for (auto addr : addresses)
+        memory[addr.to_ulong()] = val;
+}
+
 unsigned long part2()
 {
-    vector<bitset<36>> memory;
+    unordered_map<unsigned long, bitset<36>> memory;
     int idx;
     string str;
     string mask;
@@ -79,13 +121,7 @@ unsigned long part2()
         getline(ifs, str);
         if(sscanf(&str[0], "mem[%d] = %s ", &idx, &str[0]) == 2)
         {
-            bitset<36> val (stoi(str));
-            if (idx >= memory.size()) memory.resize(idx+1);
-            for (int i = 0; i < mask.size(); i++)
-            {
-                // do something different
-            }
-            memory[idx] = val;
+            populate_mem(str, idx, mask, memory);
         }
         else if (str.size() > offset)
             mask = str.substr(offset);
@@ -94,7 +130,7 @@ unsigned long part2()
 
     unsigned long sum = 0;
     for (auto val : memory)
-        sum += val.to_ulong();
+        sum += val.second.to_ulong();
 
     return sum;
 }
@@ -102,7 +138,7 @@ unsigned long part2()
 int main(int argc, char *argv[])
 {
     cout << part1() << endl; // 17481577045893
-    cout << part1() << endl; //
+    cout << part2() << endl; //
 
     return 0;
 }
