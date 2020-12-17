56ebb7a942ffda2427a949b3ae38c77ee69be670
Katie Shea <katieshea@fb.com>
2020-12-17 00:30:45 +0000

    ready for day 17

---
+++ 2020/18.cpp
@@ -0,0 +1,34 @@
+/*
+    Advent of Code Day 17
+
+    To run on a mac:
+
+        1. Update makefile "DAY" variable to 17
+        2. Run "make"
+        3. Run "./aoc"
+
+    Problem:
+
+        https://adventofcode.com/2020/day/17
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#define INPUT_FILE "input17.txt"
+
+void read_file()
+{
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+
+    read_file();
+
+    return 0;
+}
