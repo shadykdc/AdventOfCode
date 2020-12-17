66546d7f46a83acc9ef5960be11503f46e46d261
Katie Shea <katieshea@fb.com>
2020-12-17 09:38:05 +0000

    part 1

--- 2020/17.cpp
+++ 2020/17.cpp
@@ -19,16 +19,192 @@
 using namespace std;
 
 #define INPUT_FILE "input17.txt"
+#define CYCLES 6
 
-void read_file()
+class Point
 {
+    int x, y, z;
+public:
+    Point(int x, int y, int z);
+    bool operator==(const Point &p) const
+    {
+        return x == p.x && y == p.y && z == p.z;
+    }
+    bool is_neighbor(int _x, int _y, int _z)
+    {
+        return abs(x - _x) <= 1 && abs(y - _y) <= 1 && abs(z - _z) <= 1;
+    }
+};
+
+Point::Point(int _x, int _y, int _z)
+{
+    x = _x;
+    y = _y;
+    z = _z;
+}
+
+class Space
+{
+private:
+    vector<Point> active_pts = {};
+public:
+    Space();
+    int z_min, z_max, x_min, x_max, y_min, y_max;
+    void activate_pt(int x, int y, int z);
+    bool deactivate_pt(int x, int y, int z);
+    bool pt_is_active(int x, int y, int z);
+    size_t count_active_nearby(int x, int y, int z);
+    size_t get_active_count() { return active_pts.size(); }
+    void print();
+};
+
+Space::Space()
+{
+    vector<Point> active_pts = {};
+    z_min = 0;
+    z_max = 0;
+    x_min = 0;
+    x_max = 0;
+    y_min = 0;
+    y_max = 0;
+}
+
+void Space::print()
+{
+    cout << "----Space----" << endl;
+    for (int z = z_min; z <= z_max; z++)
+    {
+        cout << "z=" << z << endl;
+        for (int y = y_min; y <= y_max; y++)
+        {
+            for (int x = x_min; x <= x_max; x++)
+            {
+                if (pt_is_active(x, y, z)) cout << "#";
+                else cout << ".";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
+
+void Space::activate_pt(int x, int y, int z)
+{
+    if (pt_is_active(x, y, z)) return;
+    Point p(x, y, z);
+    active_pts.push_back(p);
+    x_min = min(x_min, x);
+    x_max = max(x_max, x);
+    y_min = min(y_min, y);
+    y_max = max(y_max, y);
+    z_min = min(z_min, z);
+    z_max = max(z_max, z);
+}
+
+bool Space::pt_is_active(int x, int y, int z)
+{
+    Point p(x, y, z);
+    for (auto point : active_pts)
+    {
+        if (point == p) return true;
+    }
+    return false;
+}
+
+// returns false if point didn't exist
+bool Space::deactivate_pt(int x, int y, int z)
+{
+    Point p(x, y, z);
+    for (int i = 0; i < active_pts.size(); i++)
+    {
+        if (active_pts[i] == p)
+        {
+            active_pts.erase(active_pts.begin()+i);
+            return true;
+        }
+    }
+    return false;
+}
+
+size_t Space::count_active_nearby(int px, int py, int pz)
+{
+    size_t count = 0;
+    for (int x = px-1; x <= px+1; x++)
+    {
+        for (int y = py-1; y <= py+1; y++)
+        {
+            for (int z = pz-1; z <= pz+1; z++)
+            {
+                if (x == px && y == py && z == pz ) continue;
+                if (pt_is_active(x, y, z)) count++;
+            }
+        }
+    }
+    return count;
+}
+
+void read_file(vector<string>& grid, Space* space)
+{
+    string row;
+    ifstream ifs(INPUT_FILE, ifstream::in);
+
+    while(ifs.good())
+    {
+        getline(ifs, row);
+        if (row.size() > 0)
+            grid.push_back(row);
+    }
+    ifs.close();
+
+    for (int y = 0; y < grid.size(); y++)
+    {
+        for (int x = 0; x < grid[0].size(); x++)
+        {
+            if (grid[y][x] == '#')
+            {
+                space->activate_pt(x, y, 0);
+            }
+        }
+    }
+
     return;
+}
+
+void iterate(Space* space)
+{
+    Space new_space = *space;
+    for (int x = space->x_min-1; x <= space->x_max+1; x++)
+    {
+        for (int y = space->y_min-1; y <= space->y_max+1; y++)
+        {
+            for (int z = space->z_min-1; z <= space->z_max+1; z++)
+            {
+                size_t count = space->count_active_nearby(x, y, z);
+                if (space->pt_is_active(x, y, z) && count != 2 && count != 3)
+                    new_space.deactivate_pt(x, y, z);
+                else if (count == 3)
+                    new_space.activate_pt(x, y, z);
+            }
+        }
+    }
+    *space = new_space;
+}
+
+int part1(Space* space)
+{
+    for (int i = 0; i < CYCLES; i++)
+        iterate(space);
+    return space->get_active_count();
 }
 
 int main(int argc, char *argv[])
 {
+    vector<string> grid;
+    Space space;
 
-    read_file();
+    read_file(grid, &space);
+
+    cout << "Part 1: " << part1(&space) << endl;
 
     return 0;
 }
