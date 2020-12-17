4708ccc6fc446f7bb9e37a0c3ccdd03cff62deb6
Katie Shea <katieshea@fb.com>
2020-12-17 00:31:02 +0000

    fix day 18

--- 2020/18.cpp
+++ 2020/18.cpp
@@ -1,15 +1,15 @@
 /*
-    Advent of Code Day 17
+    Advent of Code Day 18
 
     To run on a mac:
 
-        1. Update makefile "DAY" variable to 17
+        1. Update makefile "DAY" variable to 18
         2. Run "make"
         3. Run "./aoc"
 
     Problem:
 
-        https://adventofcode.com/2020/day/17
+        https://adventofcode.com/2020/day/18
 */
 
 #include <iostream>
