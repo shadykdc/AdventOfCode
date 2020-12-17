e8502de05640e2913d8eeafdc9b77c09c3d64644
Katie Shea <katieshea@fb.com>
2020-12-17 09:59:15 +0000

    part 2 works but is slow

--- 2020/17.1.cpp
+++ 2020/17.1.cpp
@@ -23,26 +23,24 @@ using namespace std;
 
 class Point
 {
-    int x, y, z, w;
+    int x, y, z;
 public:
-    Point(int x, int y, int z, int w);
+    Point(int x, int y, int z);
     bool operator==(const Point &p) const
     {
-        return x == p.x && y == p.y && z == p.z && w == p.w;
+        return x == p.x && y == p.y && z == p.z;
     }
-    bool is_neighbor(int _x, int _y, int _z, int _w)
+    bool is_neighbor(int _x, int _y, int _z)
     {
-        return abs(x - _x) <= 1 && abs(y - _y) <= 1 &&
-               abs(z - _z) <= 1 && abs(w - _w) <= 1;
+        return abs(x - _x) <= 1 && abs(y - _y) <= 1 && abs(z - _z) <= 1;
     }
 };
 
-Point::Point(int _x, int _y, int _z, int _w)
+Point::Point(int _x, int _y, int _z)
 {
     x = _x;
     y = _y;
     z = _z;
-    w = _w;
 }
 
 class Space
@@ -51,12 +49,13 @@ private:
     vector<Point> active_pts = {};
 public:
     Space();
-    int z_min, z_max, x_min, x_max, y_min, y_max, w_min, w_max;
-    bool activate_pt(int x, int y, int z, int w);
-    bool deactivate_pt(int x, int y, int z, int w);
-    bool pt_is_active(int x, int y, int z, int w);
-    size_t count_active_nearby(int x, int y, int z, int w);
+    int z_min, z_max, x_min, x_max, y_min, y_max;
+    bool activate_pt(int x, int y, int z);
+    bool deactivate_pt(int x, int y, int z);
+    bool pt_is_active(int x, int y, int z);
+    size_t count_active_nearby(int x, int y, int z);
     size_t get_active_count() { return active_pts.size(); }
+    void print();
 };
 
 Space::Space()
@@ -68,15 +67,32 @@ Space::Space()
     x_max = 0;
     y_min = 0;
     y_max = 0;
-    w_min = 0;
-    w_max = 0;
+}
+
+void Space::print()
+{
+    cout << "----Space----" << endl;
+    for (int z = z_min; z <= z_max; z++)
+    {
+        cout << "z=" << z << endl;
+        for (int y = y_min; y <= y_max; y++)
+        {
+            for (int x = x_min; x <= x_max; x++)
+            {
+                if (pt_is_active(x, y, z)) cout << "#";
+                else cout << ".";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 }
 
 // returns false if point already existed
-bool Space::activate_pt(int x, int y, int z, int w)
+bool Space::activate_pt(int x, int y, int z)
 {
-    if (pt_is_active(x, y, z, w)) return false;;
-    Point p(x, y, z, w);
+    if (pt_is_active(x, y, z)) return false;;
+    Point p(x, y, z);
     active_pts.push_back(p);
     x_min = min(x_min, x);
     x_max = max(x_max, x);
@@ -84,14 +100,12 @@ bool Space::activate_pt(int x, int y, int z, int w)
     y_max = max(y_max, y);
     z_min = min(z_min, z);
     z_max = max(z_max, z);
-    w_min = min(w_min, w);
-    w_max = max(w_max, w);
     return true;
 }
 
-bool Space::pt_is_active(int x, int y, int z, int w)
+bool Space::pt_is_active(int x, int y, int z)
 {
-    Point p(x, y, z, w);
+    Point p(x, y, z);
     for (auto point : active_pts)
     {
         if (point == p) return true;
@@ -100,9 +114,9 @@ bool Space::pt_is_active(int x, int y, int z, int w)
 }
 
 // returns false if point didn't exist
-bool Space::deactivate_pt(int x, int y, int z, int w)
+bool Space::deactivate_pt(int x, int y, int z)
 {
-    Point p(x, y, z, w);
+    Point p(x, y, z);
     for (int i = 0; i < active_pts.size(); i++)
     {
         if (active_pts[i] == p)
@@ -114,7 +128,7 @@ bool Space::deactivate_pt(int x, int y, int z, int w)
     return false;
 }
 
-size_t Space::count_active_nearby(int px, int py, int pz, int pw)
+size_t Space::count_active_nearby(int px, int py, int pz)
 {
     size_t count = 0;
     for (int x = px-1; x <= px+1; x++)
@@ -123,13 +137,8 @@ size_t Space::count_active_nearby(int px, int py, int pz, int pw)
         {
             for (int z = pz-1; z <= pz+1; z++)
             {
-                for (int w = pw-1; w <= pw+1; w++)
-                {
-                    if (x == px && y == py && z == pz && w == pw )
-                        continue;
-                    if (pt_is_active(x, y, z, w))
-                        count++;
-                }
+                if (x == px && y == py && z == pz ) continue;
+                if (pt_is_active(x, y, z)) count++;
             }
         }
     }
@@ -155,7 +164,7 @@ void read_file(vector<string>& grid, Space* space)
         {
             if (grid[y][x] == '#')
             {
-                space->activate_pt(x, y, 0, 0);
+                space->activate_pt(x, y, 0);
             }
         }
     }
@@ -172,24 +181,21 @@ void iterate(Space* space)
         {
             for (int z = space->z_min-1; z <= space->z_max+1; z++)
             {
-                for (int w = space->w_min-1; w <= space->w_max+1; w++)
+                size_t count = space->count_active_nearby(x, y, z);
+                if (space->pt_is_active(x, y, z))
                 {
-                    size_t count = space->count_active_nearby(x, y, z, w);
-                    if (space->pt_is_active(x, y, z, w))
-                    {
-                        if (count != 2 && count != 3)
-                            new_space.deactivate_pt(x, y, z, w);
-                    }
-                    else if (count == 3)
-                        new_space.activate_pt(x, y, z, w);
+                    if (count != 2 && count != 3)
+                        new_space.deactivate_pt(x, y, z);
                 }
+                else if (count == 3)
+                    new_space.activate_pt(x, y, z);
             }
         }
     }
     *space = new_space;
 }
 
-int part2(Space* space)
+int part1(Space* space)
 {
     for (int i = 0; i < CYCLES; i++)
         iterate(space);
@@ -203,7 +209,7 @@ int main(int argc, char *argv[])
 
     read_file(grid, &space);
 
-    cout << "Part 2: " << part2(&space) << endl; // 424
+    cout << "Part 1: " << part1(&space) << endl; // 424
 
     return 0;
 }
