32eec0221ba9034ca3dc417f3b372084924387ec
Katie Shea <katieshea@fb.com>
2020-12-23 20:10:34 +0000

    ok I did it

--- 2020/23.cpp
+++ 2020/23.cpp
@@ -17,82 +17,98 @@
 #include <list>
 #include <algorithm>
 #include <iterator>
-#include <vector>
 #include <unordered_map>
 
 using namespace std;
 
 #define INPUT_FILE "input23.txt"
+#define CUPS_TO_MOVE 3
+#define MAGIC_CRAB_NUM 1000000
 
 class CupGame
 {
-public:
-    list<long> cups;
+private:
     list<long>::iterator current_cup;
-    unordered_map<long, list<long>::iterator> map;
     long min_cup_label;
     long max_cup_label;
-    CupGame(list<long> _cups, bool to_a_million)
+
+    // we are about to move these so they're invalid as destinations
+    bool invalid_dest(int dest_label)
+    {
+        bool invalid = false;
+        auto check_cup = (*current_cup == cups.back())
+            ? cups.begin()
+            : next(current_cup);
+        for (int i = 0; i < CUPS_TO_MOVE; i++)
+        {
+            if (dest_label == *check_cup)
+                invalid = true;
+            check_cup =
+                (*check_cup == cups.back())
+                ? cups.begin()
+                : next(check_cup);
+        }
+        return invalid;
+    };
+
+public:
+    list<long> cups;
+    unordered_map<long, list<long>::iterator> map;
+
+    CupGame(list<long> _cups, bool _to_a_million)
     {
         cups = _cups;
-        min_cup_label = LONG_MAX;
-        max_cup_label = LONG_MIN;
-        current_cup = cups.begin();
-
+        min_cup_label = 1;
         auto cup = cups.begin();
         for (auto num : cups)
         {
             map[num] = cup;
             cup = next(cup);
         }
-        if (to_a_million)
+        if (_to_a_million)
         {
             long num = cups.size() + 1;
-            while(cups.size() != 1000000)
+            while(cups.size() != MAGIC_CRAB_NUM)
             {
                 cups.push_back(num);
                 map[num] = prev(cups.end());
                 num++;
             }
         }
-        for (auto cup : cups)
-        {
-            min_cup_label = min(min_cup_label, cup);
-            max_cup_label = max(max_cup_label, cup);
-        }
+        current_cup = cups.begin();
+        max_cup_label = cups.size();
     };
+
     void Move()
     {
-        // step 1 - get three cups
-        vector<long> three_cups = {};
-        for (int i = 0; i < 3; i++)
-        {
-            list<long>::iterator next_node =
-                ( *current_cup == cups.back() )
-                ? cups.begin()
-                : next(current_cup);
-            three_cups.push_back(*next_node);
-            map.erase(*next_node);
-            cups.erase(next_node);
-        }
-        // step 2 - select a destination cup
+        // step 1 - select a destination cup
         long dest_label = *current_cup - 1;
-        auto got = map.find(dest_label);
-        while (got == map.end())
+        while (dest_label < min_cup_label || invalid_dest(dest_label))
         {
             dest_label--;
             if (dest_label < min_cup_label)
                 dest_label = max_cup_label;
-            got = map.find(dest_label);
         }
-        // step 3 - place cups immediately cw of dest_idx
-        auto dest_cup = got->second;
-        auto next_cup = next(dest_cup);
-        cups.insert(next_cup, three_cups.begin(), three_cups.end());
-        for (size_t i = 0; i < 3; i++)
+        auto got = map.find(dest_label);
+        if (got == map.end())
+            throw("Couldn't find dest_label in map.");
+        auto dest_cup =
+            (got->first == cups.back())
+            ? cups.begin()
+            : next(got->second);
+        // step 2 - place move cups immediately cw of the destination cup
+        // (except splice places things before so... ccw of destination + 1)
+        auto move_cup =
+            (*current_cup == cups.back())
+            ? cups.begin()
+            : next(current_cup);
+        for (int i = 0; i < CUPS_TO_MOVE; i++)
         {
-            dest_cup = (*dest_cup == cups.back()) ? cups.begin() : next(dest_cup);
-            map[*dest_cup] = dest_cup;
+            cups.splice(dest_cup, cups, move_cup);
+            move_cup =
+                (*current_cup == cups.back())
+                ? cups.begin()
+                : next(current_cup);
         }
         // step 4 - get next cup
         current_cup =
@@ -100,24 +116,21 @@ public:
                 ? cups.begin()
                 : next(current_cup);
     };
+
     void PrintFrom1()
     {
         auto one = map.find(1);
-        if (one == map.end()) throw("couldn't find 1");
-        auto print_me = (one->first == cups.back())
+        if (one == map.end())
+            throw("couldn't find 1");
+        auto print_me =
+            (one->first == cups.back())
             ? cups.begin()
             : next(one->second);
         while (print_me != cups.end())
-        {
-            cout << *print_me;
-            print_me++;
-        }
+            cout << *print_me++;
         print_me = cups.begin();
         while (*print_me != 1)
-        {
-            cout << *print_me;
-            print_me++;
-        }
+            cout << *print_me++;
         cout << endl;
     };
 
@@ -148,25 +161,27 @@ void part2(CupGame* cups, size_t moves)
 {
     for (size_t i = 0; i < moves; i++)
         cups->Move();
-
     auto one = cups->map.find(1);
-    if (one == cups->map.end()) throw("couldn't find 1");
-    auto two = (1 == cups->cups.back()) ? cups->cups.begin() : next(one->second);
-    auto three = (*two == cups->cups.back()) ? cups->cups.begin() : next(two);
-    cout << *(one->second) << " " << *two << " " << *three << endl;
+    if (one == cups->map.end())
+        throw("couldn't find 1");
+    auto two =
+        (1 == cups->cups.back())
+        ? cups->cups.begin()
+        : next(one->second);
+    auto three =
+        (*two == cups->cups.back())
+        ? cups->cups.begin()
+        : next(two);
     cout << "Part 2: " << *two * *three << endl;
 }
 
 int main(int argc, char *argv[])
 {
-    // my input
-    // CupGame cups1({9,4,2,3,8,7,6,1,5}, false);
-    // example, p1: 67384529, p2: 149245887792
-    CupGame cups1({3,8,9,1,2,5,4,6,7}, false);
+    list<long> input = {9,4,2,3,8,7,6,1,5};
+    CupGame cups1(input, false);
     part1(&cups1, 100); // 36542897
-
-    CupGame cups2({9,4,2,3,8,7,6,1,5}, true);
-    part2(&cups2, 20); //
+    CupGame cups2(input, true);
+    part2(&cups2, 10000000); // 562136730660
 
     return 0;
 }
