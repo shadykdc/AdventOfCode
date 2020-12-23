c339efe18184712ac18d7562790263701a155b23
Katie Shea <katieshea@fb.com>
2020-12-23 15:48:11 +0000

    now with a list

--- 2020/23.cpp
+++ 2020/23.cpp
@@ -14,6 +14,9 @@
 
 #include <iostream>
 #include <fstream>
+#include <list>
+#include <algorithm>
+#include <iterator>
 #include <vector>
 
 using namespace std;
@@ -23,12 +26,11 @@ using namespace std;
 class CupGame
 {
 public:
-    vector<int> cups;
-    int current_idx;
-    int current_label;
+    list<int> cups;
+    list<int>::iterator current_cup;
     int min_cup_label;
     int max_cup_label;
-    CupGame(vector<int> _cups, bool to_a_million)
+    CupGame(list<int> _cups, bool to_a_million)
     {
         cups = _cups;
         min_cup_label = INT_MAX;
@@ -38,7 +40,7 @@ public:
             min_cup_label = min(min_cup_label, cup);
             max_cup_label = max(max_cup_label, cup);
         }
-        current_idx = 0;
+        current_cup = cups.begin();
         if (to_a_million)
         {
             int num = cups.size() + 1;
@@ -51,76 +53,67 @@ public:
     };
     void Move()
     {
-        current_label = cups[current_idx];
         // step 1 - get three cups
-        vector<int> three_cups;
-        int idx = (current_idx + 1) % cups.size();
+        vector<int> three_cups = {};
         while (three_cups.size() < 3)
         {
-            three_cups.push_back(cups[idx]);
-            cups.erase(cups.begin() + idx);
-            // remember cups size changes in this loop
-            for (int i = 0; i < cups.size(); i++)
-            {
-                if (cups[i] == current_label)
-                    current_idx = i;
-            }
-            idx = (current_idx + 1) % cups.size();
+            list<int>::iterator next_node =
+                ( *current_cup == cups.back() )
+                ? cups.begin()
+                : next(current_cup);
+            three_cups.push_back(*next_node);
+            cups.erase(next_node);
         }
+        cout << "pick up ";
+        for (auto cup : three_cups) cout << cup << " ";
+        cout << endl;
         // step 2 - select a destination cup
-        int dest_label = cups[current_idx];
-        int dest_idx = -1;
-        while (dest_idx == -1)
+        int dest_label = *current_cup - 1;
+        auto got = find(cups.begin(), cups.end(), dest_label);
+        while (got == cups.end())
         {
             dest_label--;
             if (dest_label < min_cup_label)
                 dest_label = max_cup_label;
-            for (int i = 0; i < cups.size(); i++)
-            {
-                if (cups[i] == dest_label)
-                {
-                    dest_idx = i + 1;
-                    break;
-                }
-            }
+            got = find(cups.begin(), cups.end(), dest_label);
         }
+        cout << "destination: " << dest_label << endl;
         // step 3 - place cups immediately cw of dest_idx
-        cups.insert(cups.begin() + dest_idx, three_cups.begin(), three_cups.end());
+        auto next_cup = (*got == cups.back()) ? cups.begin() : next(got);
+        cups.insert(next_cup, three_cups.begin(), three_cups.end());
         // step 4 - find idx of number in front of current_label
-        for (int i = 0; i < cups.size(); i++)
-        {
-            if (cups[i] == current_label)
-            {
-                current_idx = (i + 1) % cups.size();
-                break;
-            }
-        }
+        current_cup =
+                ( *current_cup == cups.back() )
+                ? cups.begin()
+                : next(current_cup);
     };
 
     void PrintFrom1()
     {
         // find 1
-        int idx_of_one = -1;
-        for (int i = 0; i < cups.size(); i++)
+        auto one = find(cups.begin(), cups.end(), 1);
+        if (one == cups.end()) return;
+        auto print_me = (*one == cups.back()) ? cups.begin() : next(one);
+        while (print_me != cups.end())
         {
-            if (cups[i] == 1)
-                idx_of_one = i;
+            cout << *print_me;
+            print_me++;
         }
-
-        for (int i = idx_of_one + 1; i < (cups.size() + idx_of_one - 1); i++)
+        print_me = cups.begin();
+        while (*print_me != *one)
         {
-            int j = i % cups.size();
-            cout << cups[j];
+            cout << *print_me;
+            print_me++;
         }
         cout << endl;
     };
 
     void Print()
     {
-        for (int i = 0; i < cups.size(); i++)
+        for (auto cup = cups.begin(); cup != cups.end(); cup++)
         {
-            if (i == current_idx) cout << "(" << cups[i] << ") ";
-            else cout << cups[i] << " ";
+            if (*cup == *current_cup) cout << "(" << *cup << ") ";
+            else cout << *cup << " ";
         }
         cout << endl;
     };
@@ -128,11 +121,11 @@ public:
 
 void part1(CupGame* cups, int moves)
 {
-    // cups->Print();
+    cups->Print();
     for (int i = 0; i < moves; i++)
     {
         cups->Move();
-        // cups->Print();
+        cups->Print();
     }
     cout << "Part 1: ";
     cups->PrintFrom1();
@@ -146,23 +139,19 @@ void part2(CupGame* cups, int moves)
         cups->Move();
     }
     // find 1
-    int idx_of_one = -1;
-    for (int i = 0; i < cups->cups.size(); i++)
-    {
-        if (cups->cups[i] == 1)
-            idx_of_one = i;
-    }
-    cout << cups->cups[ (idx_of_one + 1) % cups->cups.size() ] *
-            cups->cups[ (idx_of_one + 2) % cups->cups.size() ]
-         << endl;
+    auto one = find(cups->cups.begin(), cups->cups.end(), 1);
+    auto two =   (*one == cups->cups.back()) ? cups->cups.begin() : next(one);
+    auto three = (*two == cups->cups.back()) ? cups->cups.begin() : next(two);
+    cout << *two * *three << endl;
 }
 
 int main(int argc, char *argv[])
 {
     CupGame cups1({9,4,2,3,8,7,6,1,5}, false);
+    // CupGame cups1({3,8,9,1,2,5,4,6,7}, false);
     part1(&cups1, 100); // 36542897
     CupGame cups2({9,4,2,3,8,7,6,1,5}, true);
-    part2(&cups2, 10000000); //
+    // part2(&cups2, 10000000); //
     cout << endl;
 
     return 0;
