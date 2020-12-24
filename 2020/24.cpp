b6a4aef7ef8e3274437e531f50c18a4fb30003c5
Katie Shea <katieshea@fb.com>
2020-12-24 12:27:10 +0000

    day 24

--- 2020/24.cpp
+++ 2020/24.cpp
@@ -21,8 +21,6 @@
 using namespace std;
 
 #define INPUT_FILE "input24.txt"
-#define X_INCR 0.5   // cos(60*PI/180);
-#define Y_INCR 0.866 // ...02540378; // sin(60*PI/180);
 
 void read_file(vector<string>& tiles)
 {
@@ -42,66 +40,58 @@ void read_file(vector<string>& tiles)
 
 struct pair_hash
 {
-    size_t operator () (pair<float, float> const &pair) const
+    size_t operator () (pair<int, int> const &pair) const
     {
-        size_t h1 = hash<float>()(pair.first);
-        size_t h2 = hash<float>()(pair.second);
+        size_t h1 = hash<int>()(pair.first);
+        size_t h2 = hash<int>()(pair.second);
 
         return h1 ^ h2;
     }
 };
 
-float rnd(float n)
-{
-    // round to nearest rths
-    const float r = 100.0;
-    n = ceil(n * r) / r;
-    return n;
-}
-
-void get_x_y(string& dirs, float* x, float* y,
-            unordered_set<pair<float, float>, pair_hash>& white_tiles)
+void get_x_y(string& dirs, int* x, int* y,
+            unordered_set<pair<int, int>, pair_hash>& white_tiles)
 {
     for (size_t i = 0; i < dirs.size(); i++)
     {
         if (dirs[i] == 'n' || dirs[i] == 's')
         {
             if (dirs[i] == 'n')
-                *y += Y_INCR;
+                *y += 1;
             else if (dirs[i] == 's')
-                *y -= Y_INCR;
+                *y -= 1;
             if (i == dirs.size()-1)
                 throw("problem");
             i++;
             if (dirs[i] == 'w')
-                *x -= X_INCR;
+                *x -= 1;
             else if (dirs[i] == 'e')
-                *x += X_INCR;
+                *x += 1;
             else
                 throw("problem");
         }
         else if (dirs[i] == 'w')
-            *x -= 1;
+            *x -= 2;
         else if (dirs[i] == 'e')
-            *x += 1;
+            *x += 2;
         else
             throw("problem");
-        pair<float, float> coord = make_pair(rnd(*x), rnd(*y));
+        pair<int, int> coord = make_pair(*x, *y);
         white_tiles.insert(coord);
     }
 }
 
 void get_tiles(vector<string>& tiles,
-    unordered_set<pair<float, float>, pair_hash>& black_tiles,
-    unordered_set<pair<float, float>, pair_hash>& white_tiles)
+    unordered_set<pair<int, int>, pair_hash>& black_tiles,
+    unordered_set<pair<int, int>, pair_hash>& white_tiles)
 {
     for (auto dirs : tiles)
     {
-        float x = 0;
-        float y = 0;
+        int x = 0;
+        int y = 0;
         get_x_y(dirs, &x, &y, white_tiles);
 
-        pair<float, float> coord = make_pair(rnd(x), rnd(y));
+        pair<int, int> coord = make_pair(x, y);
         auto got = black_tiles.find(coord);
         if (got == black_tiles.end())
         {
@@ -120,47 +110,64 @@ void get_tiles(vector<string>& tiles,
 size_t part1(vector<string>& tiles)
 {
     // coordinates of black tiles
-    unordered_set<pair<float, float>, pair_hash> black_tiles;
-    unordered_set<pair<float, float>, pair_hash> white_tiles;
+    unordered_set<pair<int, int>, pair_hash> black_tiles;
+    unordered_set<pair<int, int>, pair_hash> white_tiles;
     get_tiles(tiles, black_tiles, white_tiles);
-    cout << "white: " << white_tiles.size() << endl;
     return black_tiles.size();
 }
 
-size_t count_b_neighbors(pair<float, float> coord,
-    unordered_set<pair<float, float>, pair_hash>& black_tiles)
+size_t count_b_neighbors(pair<int, int> coord,
+    unordered_set<pair<int, int>, pair_hash>& black_tiles,
+    unordered_set<pair<int, int>, pair_hash>& white_tiles)
 {
     size_t count = 0;
-    coord.first = rnd(coord.first - 1); // w
-    if (black_tiles.find(coord) != black_tiles.end()) count++;
-    coord.first = rnd(coord.first + X_INCR);
-    coord.second = rnd(coord.second + Y_INCR); // nw
-    if (black_tiles.find(coord) != black_tiles.end()) count++;
-    coord.first = rnd(coord.first + 1); // ne
-    if (black_tiles.find(coord) != black_tiles.end()) count++;
-    coord.first = rnd(coord.first + X_INCR);
-    coord.second = rnd(coord.second - Y_INCR); // e
-    if (black_tiles.find(coord) != black_tiles.end()) count++;
-    coord.first = rnd(coord.first - X_INCR);
-    coord.second = rnd(coord.second - Y_INCR); // se
-    if (black_tiles.find(coord) != black_tiles.end()) count++;
-    coord.first = rnd(coord.first - 1); // sw
-    if (black_tiles.find(coord) != black_tiles.end()) count++;
-    coord.first = rnd(coord.first - X_INCR);
-    coord.second = rnd(coord.second + Y_INCR);
+
+    coord.first -= 2; // w
+    if (black_tiles.find(coord) != black_tiles.end())
+        count++;
+    else
+        white_tiles.insert(coord);
+    coord.first += 1;
+    coord.second += 1; // nw
+    if (black_tiles.find(coord) != black_tiles.end())
+        count++;
+    else
+        white_tiles.insert(coord);
+    coord.first += 2; // ne
+    if (black_tiles.find(coord) != black_tiles.end())
+        count++;
+    else
+        white_tiles.insert(coord);
+    coord.first += 1;
+    coord.second -= 1; // e
+    if (black_tiles.find(coord) != black_tiles.end())
+        count++;
+    else
+        white_tiles.insert(coord);
+    coord.first -= 1;
+    coord.second -= 1; // se
+    if (black_tiles.find(coord) != black_tiles.end())
+        count++;
+    else
+        white_tiles.insert(coord);
+    coord.first -= 2; // sw
+    if (black_tiles.find(coord) != black_tiles.end())
+        count++;
+    else
+        white_tiles.insert(coord);
     return count;
 }
 
 void flip_tiles(
-    unordered_set<pair<float, float>, pair_hash>& black_tiles,
-    unordered_set<pair<float, float>, pair_hash>& white_tiles)
+    unordered_set<pair<int, int>, pair_hash>& black_tiles,
+    unordered_set<pair<int, int>, pair_hash>& white_tiles)
 {
-    unordered_set<pair<float, float>, pair_hash> next_black_tiles;
-    unordered_set<pair<float, float>, pair_hash> next_white_tiles;
+    unordered_set<pair<int, int>, pair_hash> next_black_tiles;
+    unordered_set<pair<int, int>, pair_hash> next_white_tiles;
 
     for (auto b_coord : black_tiles)
     {
-        size_t neighbors = count_b_neighbors(b_coord, black_tiles);
+        size_t neighbors = count_b_neighbors(b_coord, black_tiles, white_tiles);
         if (neighbors == 0 || neighbors > 2)
             next_white_tiles.insert(b_coord);
         else
@@ -168,7 +175,7 @@ void flip_tiles(
     }
     for (auto w_coord : white_tiles)
     {
-        size_t neighbors = count_b_neighbors(w_coord, black_tiles);
+        size_t neighbors = count_b_neighbors(w_coord, black_tiles, white_tiles);
         if (neighbors == 2)
             next_black_tiles.insert(w_coord);
         else
@@ -181,18 +188,12 @@ void flip_tiles(
 size_t part2(vector<string>& tiles, size_t iterations)
 {
     // coordinates of black tiles
-    unordered_set<pair<float, float>, pair_hash> black_tiles;
-    unordered_set<pair<float, float>, pair_hash> white_tiles;
+    unordered_set<pair<int, int>, pair_hash> black_tiles;
+    unordered_set<pair<int, int>, pair_hash> white_tiles;
     get_tiles(tiles, black_tiles, white_tiles);
 
-    cout << endl << "Day " << 0 << ": " << black_tiles.size() << " " << white_tiles.size() << endl;
-
     for (int i = 1; i < iterations+1; i++)
-    {
         flip_tiles(black_tiles, white_tiles);
-        if (i < 10 || i % 10 == 0)
-            cout << "Day " << i << ": " << black_tiles.size() << " " << white_tiles.size() << endl;
-    }
 
     return black_tiles.size();
 }
@@ -203,7 +204,7 @@ int main(int argc, char *argv[])
     read_file(tiles);
 
     cout << "Part 1: " << part1(tiles) << endl; // 351
-    cout << "Part 2: " << part2(tiles, 100) << endl;
+    cout << "Part 2: " << part2(tiles, 100) << endl; // 3869
 
     return 0;
 }
