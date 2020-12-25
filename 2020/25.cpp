ead33cb7b4018714c6559f1f81bcc2edc7d21ab7
Katie Shea <katieshea@fb.com>
2020-12-25 09:56:55 +0000

    part 1

--- 2020/25.cpp
+++ 2020/25.cpp
@@ -20,22 +20,44 @@
 using namespace std;
 
 #define INPUT_FILE "input25.txt"
+#define TRANSFORM 20201227
 
-void read_file()
+long get_loop_size(long key)
 {
-    return;
+    long subject_num = 7;
+    long value = 1;
+    long loop_size = 0;
+    while (value != key)
+    {
+        value = (value * subject_num) % TRANSFORM;
+        loop_size++;
+    }
+    return loop_size;
 }
 
-int part1()
+long transform(long loop_size, long subject_num)
 {
-    return 1;
+    long value = 1;
+    for (long i = 0; i < loop_size; i++)
+        value = (value * subject_num) % TRANSFORM;
+    return value;
 }
 
 int main(int argc, char *argv[])
 {
-    read_file();
+    // input: the two public keys
+    const long card_public_key = 8421034;
+    const long door_public_key = 15993936;
 
-    cout << "Part 1: " << part1() << endl;
+    long card_loop_size = get_loop_size(card_public_key);
+    cout << card_loop_size << endl;
+    long door_loop_size = get_loop_size(door_public_key);
+    cout << door_loop_size << endl;
+
+    long card_encryption_key = transform(door_loop_size, card_public_key);
+    long door_encryption_key = transform(card_loop_size, door_public_key);
+
+    cout << "Part 1: " << card_encryption_key << " " << door_encryption_key << endl;
 
     return 0;
 }
