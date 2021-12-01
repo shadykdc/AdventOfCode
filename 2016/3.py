610459c7a978f46f65afbdeccaf05a2e76d91977
Katie Shea <katieshea@fb.com>
2021-12-01 23:15:22 +0000

    2016 day 3

---
+++ 2016/3.py
@@ -0,0 +1,34 @@
+with open('input3.txt', 'r') as f:
+    lines = [[int(num) for num in line.strip().split()] for line in f.readlines()]
+
+example = [[5, 10, 25]]
+
+def part_one(rows):
+    return len([row for row in rows if sum(row) - 2 * max(row) > 0])
+
+assert(part_one(example) == 0)
+print(f"Part 1: {part_one(lines)}")
+assert(part_one(lines) == 869)
+
+example2 = [
+    [101, 301, 501],
+    [102, 302, 502],
+    [103, 303, 503],
+    [201, 401, 601],
+    [202, 402, 602],
+    [203, 403, 603],
+]
+
+def part_two(rows):
+    count = 0
+    for r in range(int(len(rows)/3)):
+        r *= 3
+        for c in range(3):
+            sides = [rows[r][c], rows[r+1][c], rows[r+2][c]]
+            if sum(sides) - max(sides) * 2 > 0:
+                count += 1
+    return count
+
+assert(part_two(example2) == 6)
+print(f"Part 2: {part_two(lines)}")
+assert(part_two(lines) == 1544)
---
+++ 2021/2.py
@@ -0,0 +1,19 @@
+with open('input2.txt', 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+example = []
+
+def part_one(lines):
+    return 0
+
+assert(part_one(example) == 0)
+print(f"Part 1: {part_one(lines)}")
+assert(part_one(lines) == 0)
+
+
+def part_two(lines):
+    return 0
+
+assert(part_two(example) == 0)
+print(f"Part 2: {part_two(lines)}")
+assert(part_two(lines) == 0)
