5aabd11d4947c4ac5151103660c1e09d78df88f6
Katie Shea <katieshea@fb.com>
2021-12-01 21:17:59 +0000

    day 7

--- 2017/7.py
+++ 2017/7.py
@@ -1,5 +1,5 @@
 import re
-from typing import Dict, List, Set
+from typing import Dict, List, Set, Tuple
 
 class Program:
     def __init__(self, name: str, weight: int, programs: List[str] = []) -> None:
@@ -8,46 +8,53 @@ class Program:
         self.programs: List[str] = programs
 
     def total_weight(self, tower: Dict[str, "Program"]) -> int:
-        return sum([tower[prog].weight for prog in programs]) + self.weight
+        return sum([tower[prog].total_weight(tower) for prog in self.programs]) + self.weight
 
     def __str__(self) -> str:
         return f"{self.name} - {self.programs}"
 
-with open('input7.txt', 'r') as f:
-    lines = [line.strip() for line in f.readlines()]
+with open('input7.2.txt', 'r') as f:
+    data = [line.strip() for line in f.readlines()]
 
-tower: Dict[str, Program] = {}
-children: Set[str] = set()
-for line in lines:
-    name = line.split(" ")[0]
-    weight = int(line.split("(")[1].split(")")[0])
-    programs = []
-    if "->" in line:
-        programs = [name.strip() for name in line.split("->")[1].split(", ")]
-        for program in programs:
-            children.add(program)
-    tower[name] = Program(name, weight, programs)
+def get_tower(lines) -> Tuple[Dict[str, Program], Set[str]]:
+    tower: Dict[str, Program] = {}
+    children: Set[str] = set()
+    for line in lines:
+        name = line.split(" ")[0]
+        weight = int(line.split("(")[1].split(")")[0])
+        programs = []
+        if "->" in line:
+            programs = [name.strip() for name in line.split("->")[1].split(", ")]
+            for program in programs:
+                children.add(program)
+        tower[name] = Program(name, weight, programs)
+    return tower, children
 
-def part_one(tower: Dict[str, Program]) -> str:
+tower, children = get_tower(data)
+
+def part_one(tower: Dict[str, Program], children) -> str:
     return [key for key in tower.keys() if key not in children][0]
 
-root_name = part_one(tower)
+root_name = part_one(tower, children)
 print(f"Part 1: {root_name}")
 
-def balanced(tower: Dict[str, Program], name: str) -> bool:
-    weights = [tower[prog].total_weight(tower) for prog in tower[name].programs]
-    if len(set(weights)) != len(weights):
-        weights.sort()
-        if weights.count(weights[0]) == 1:
-            return weights[1] - weights[0]
-        end = len(weights)-1
-        if weights.count(weights[end]) == 1:
-            return weights[end] - weights[end-1]
+def weight_difference(tower: Dict[str, Program], name: str) -> int:
+    weights = [tower[child].total_weight(tower) for child in tower[name].programs]
+    if len(set(weights)) != 1:
+        for idx, weight in enumerate(weights):
+            if weights.count(weight) == 1 and len(weights) > 1:
+                program = tower[tower[name].programs[idx]]
+                if idx == 0:
+                    return weights[1] - weight + program.weight
+                return weights[0] - weight + program.weight
+    return 0
 
 
-def part_two(tower: Dict[str, Program], root_name: str) -> int:
-    if len(tower[root_name].programs) == 0:
-        return 0
-    return balaned(tower, root_name)
+def part_two(tower: Dict[str, Program], name: str) -> int:
+    for child in tower[name].programs:
+        if weight_difference(tower, child) > 0:
+            return part_two(tower, child)
+    return weight_difference(tower, name)
 
-part_two(tower, root_name)
+print(f"Part Two: {part_two(tower, root_name)}")
+assert(part_two(tower, root_name) == 1275)
