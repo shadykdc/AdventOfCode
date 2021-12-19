685e49782d785ea17a3087cf48d4cd9fafbc4463
Katie Shea <katieshea@fb.com>
2021-12-19 06:46:39 -0500

    too slow

--- 2021/19.py
+++ 2021/19.py
@@ -1,22 +1,74 @@
+import itertools
+
 def get_input(name):
     with open(name, 'r') as f:
-        return [line.strip() for line in f.readlines()]
+        lines = [line.strip() for line in f.readlines()]
+        scanners = []
+        num = 0
+        for line in lines:
+            if "---" in line:
+                num = int(line.split()[2])
+                scanners.append([])
+            elif "," in line:
+                scanners[num].append([int(num) for num in line.split(",")])
+    return scanners
 
-lines = get_input('input19.txt')
+scanners = get_input('input19.txt')
 example = get_input('input19.1.txt')
 
+RANGE = 1000
+SHARED = 12
+ORIENTATIONS = 24
 
-def part_one(lines):
-    return 0
+def check_for_overlap(scanner, beacons):
+    starts = [(x, y, z) for x in range(RANGE) for y in range(RANGE) for z in range(RANGE)]
+    for i, j, k in starts:
+        scan = {(coord[0]+i, coord[1]+j, coord[2]+k) for coord in scanner}
+        overlap = len(scan.intersection(beacons))
+        if overlap >= 12:
+            return True, i, j, k
+    return False, 0, 0, 0
 
-assert(part_one(example) == 0)
-print(f"Part 1: {part_one(lines)}")
-assert(part_one(lines) == 0)
+def rotate(coord, axis):
+    axies = [num for num in [0, 1, 2] if num != axis]
+    temp = coord[axies[0]]
+    coord[axies[0]] = coord[axies[1]]
+    coord[axies[1]] = -temp
 
+def get_orientations(scanner):
+    orientations = [[] for _ in range(ORIENTATIONS)]
+    for i in range(ORIENTATIONS):
+        for coord in scanner:
+            coord1 = [num for num in coord]
+            coord2 = [-num for num in coord]
+            for idx in range(3):
+                for _ in range(4):
+                    orientations[i].append(coord1)
+                    rotate(coord1, idx)
+                    orientations[i].append(coord2)
+                    rotate(coord2, idx)
+    return orientations
 
-def part_two(lines):
-    return 0
+def try_scanner(scanners, beacons):
+    scanner = scanners.pop()
+    for rotated in get_orientations(scanner):
+        overlap, x, y, z = check_for_overlap(rotated, beacons)
+        if overlap:
+            for i, j, k in rotated:
+                beacons.add((i+x, j+y, k+z))
+            return
+    scanners.append(scanner)
 
-assert(part_two(example) == 0)
-print(f"Part 2: {part_two(lines)}")
-assert(part_two(lines) == 0)
+def populate_beacons(scanners, beacons):
+    beacons = {tuple(coord) for coord in scanners.pop(0)}
+    while len(scanners) > 0:
+        try_scanner(scanners, beacons)
+
+def part_one(scanners):
+    scanners = [l for l in scanners]
+    beacons = set()
+    populate_beacons(scanners, beacons)
+    return len(beacons)
+
+print(f"Example: {part_one(example)}")
+print(f"Part 1: {part_one(scanners)}")
