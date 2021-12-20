1ce5178e99dc5fc7f5aee3829d936eb6f7eaef8f
Katie Shea <katieshea@fb.com>
2021-12-20 05:49:01 -0500

    20

--- 2021/20.py
+++ 2021/20.py
@@ -9,32 +9,23 @@ img_algo, img = get_input('input20.txt')
 ex_algo, ex_img = get_input('input20.1.txt')
 
 def get_number(img, i, j):
-    hashdots = []
-    for y in range(j-1, j+2):
-        for x in range(i-1, i+2):
-            if x < 0 or x >= len(img[0]) or y < 0 or y >= len(img):
-                hashdots.append(".")
-            else:
-                hashdots.append(img[y][x])
-    binstr = "".join(hashdots).replace(".", "0").replace("#", "1")
-    print(binstr)
-    return int(binstr, 2)
+    hashdots = [
+        "."
+        if x < 0 or x >= len(img[0]) or y < 0 or y >= len(img)
+        else img[y][x]
+        for y in range(j-1, j+2)
+        for x in range(i-1, i+2)
+    ]
+    return int("".join(hashdots).replace(".", "0").replace("#", "1"), 2)
 
 assert(get_number(ex_img, 2, 2) == 34)
 
-def pad_image(img):
-    for idx, row in enumerate(img):
-        img[idx] = ['.', '.'] + row + ['.', '.']
-    img.insert(0, ['.' for _ in range(len(img[0]))])
-    img.append(['.' for _ in range(len(img[0]))])
-
 def enhance(img_algo, img):
-    out_img = [['.' for ch in row] for row in img]
-    for j in range(len(img)):
-        for i in range(len(img[0])):
-            idx = get_number(img, i, j)
-            out_img[j][i] = img_algo[idx]
-    return out_img
+    return [
+        img_algo[get_number(img, i, j)]
+        for j in range(len(img))
+        for i in range(len(img[0]))
+    ]
 
 def count_lit_pixels(img):
     count = 0
