aa4916d44790d94be47c57126c7b5d8886bc986c
Katie Shea <katieshea@fb.com>
2021-12-20 10:42:23 -0500

    20

--- 2021/20.py
+++ 2021/20.py
@@ -1,8 +1,11 @@
+ZERO = '0'
+ONE = '1'
+
 def get_input(name):
     with open(name, 'r') as f:
-        line = f.readline().strip()
+        line = [ZERO if ch == "." else ONE for ch in f.readline().strip()]
         f.readline()
-        img = [[ch for ch in row.strip()] for row in f.readlines()]
+        img = [[ZERO if ch == "." else ONE for ch in row.strip()] for row in f.readlines()]
     return line, img
 
 img_algo, img = get_input('input20.txt')
@@ -10,32 +13,29 @@ ex_algo, ex_img = get_input('input20.1.txt')
 
 def print_img(img):
     for row in img:
-        print("".join(row))
+        print("".join(['.' if ch == ZERO else '#' for ch in row]))
     print(" ")
 
-def get_number(img, i, j, zero):
-    hashdots = [
-        img[y][x]
+def get_number(img, i, j, default):
+    binarystr = [
+        default if x not in range(0, len(img[0])) or y not in range(0, len(img))
+        else img[y][x]
         for y in range(j-1, j+2)
         for x in range(i-1, i+2)
     ]
-    return int("".join(hashdots).replace('.', "0").replace('#', "1"), 2)
+    return int("".join(binarystr), 2)
 
-def enhance(img_algo, img, zero):
-    return [[
-        img_algo[get_number(img, i, j, zero)]
-        for i in range(1, len(img[0])-1)
-        ] for j in range(1, len(img)-1)
+def enhance(img_algo, img, default):
+    return [
+        [img_algo[get_number(img, i, j, default)] for i in range(len(img[0]))]
+        for j in range(len(img))
     ]
 
-def count_pixels(img, val="#"):
-    count = 0
-    for row in img:
-        count += row.count(val)
-    return count
+def count_pixels(img):
+    return sum([sum([int(ch) for ch in row]) for row in img])
 
-def pad_image(img, pad=2, val="."):
-    pad = [val] * pad
+def pad_image(img, val=ZERO, times=1):
+    pad = [val] * times
     for idx, row in enumerate(img):
         img[idx] = pad + row + pad
     for _ in range(len(pad)):
@@ -45,12 +45,9 @@ def pad_image(img, pad=2, val="."):
 def solution(algo, in_img, steps=2):
     img = [[ch for ch in row] for row in in_img]
     for step in range(steps):
-        print(f"step {step}")
-        zero = '#' if not steps % 2 and algo[0] == '#' else '.'
-        pad_image(img, 2, zero)
-        print_img(img)
-        img = enhance(algo, img, zero)
-        print_img(img)
+        default = ZERO if not step % 2 or algo[0] == ZERO else ONE
+        pad_image(img, val=default)
+        img = enhance(algo, img, default)
     return count_pixels(img)
 
 assert(solution(ex_algo, ex_img, 2) == 35)
@@ -61,5 +58,4 @@ assert(p1 == 5379)
 assert(solution(ex_algo, ex_img, 50) == 3351)
 p2 = solution(img_algo, img, 50)
 print(f"Part Two: {p2}")
-assert(p2 < 19343)
-assert(p2 > 17704)
+assert(p2 == 17917)
