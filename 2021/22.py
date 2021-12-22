583e22d9b67321a7378bf82d746678ab772cf157
Katie Shea <katieshea@fb.com>
2021-12-22 11:15:27 -0500

    22

--- 2021/22.py
+++ 2021/22.py
@@ -10,26 +10,42 @@ class Cube:
         self.y2 = int(nums[3])
         self.z1 = int(nums[4])
         self.z2 = int(nums[5])
-        self.vol = self.calculate_volume()
+        self.off = []
 
     def __str__(self):
         return f"{'on' if self.on else 'off'} {self.x1} {self.x2}"
 
+    def subtract(self, cube):
+        if self.get_shared_volume(cube):
+            for off_cube in self.off:
+                off_cube.subtract(cube)
+            self.off.append(Cube(False, self.get_shared_coords(cube)))
+
     def get_coords(self):
         return [self.x1, self.x2, self.y1, self.y2, self.z1, self.z2]
 
-    def get_shared_coords(self, step):
-        # get_shared_volume must be true
-        return [max(self.x1, step.x1), min(self.x2, step.x2), max(self.y1, step.y1), min(self.y2, step.y2), max(self.z1, step.z1), min(self.z2, step.z2)]
+    def get_shared_coords(self, cube):
+        if self.get_shared_volume(cube):
+            return [
+                max(self.x1, cube.x1), min(self.x2, cube.x2),
+                max(self.y1, cube.y1), min(self.y2, cube.y2),
+                max(self.z1, cube.z1), min(self.z2, cube.z2)
+            ]
+        return None
 
     def out_of_bounds(self):
-        return abs(self.x1) > 50 or abs(self.x2) > 50 or abs(self.y1) > 50 or abs(self.y2) > 50 or abs(self.z1) > 50 or abs(self.z2) > 50
+        return abs(self.x1) > 50 or abs(self.x2) > 50 \
+            or abs(self.y1) > 50 or abs(self.y2) > 50 \
+            or abs(self.z1) > 50 or abs(self.z2) > 50
 
     def calculate_volume(self):
-        return abs(self.x1-self.x2) * abs(self.y1-self.y2) * abs(self.z1-self.z2)
+        return abs(self.x2-self.x1+1) \
+             * abs(self.y2-self.y1+1) \
+             * abs(self.z2-self.z1+1) \
+             - sum([cube.calculate_volume() for cube in self.off])
 
-    def get_shared_volume(self, step):
-        return max(min(self.x2, step.x2)-max(self.x1, step.x1), 0) * max(min(self.y2, step.y2)-max(self.y1, step.y1), 0) * max(min(self.z2, step.z2)-max(self.z1, step.z1), 0)
+    def get_shared_volume(self, cube):
+        return max(min(self.x2, cube.x2)-max(self.x1, cube.x1)+1, 0) * max(min(self.y2, cube.y2)-max(self.y1, cube.y1)+1, 0) * max(min(self.z2, cube.z2)-max(self.z1, cube.z1)+1, 0)
 
 def get_input(name):
     with open(name, 'r') as f:
@@ -64,27 +80,17 @@ assert(p1 == 612714)
 def part_two(steps):
     steps = [step for step in steps]
     cubes = []
-    on_count = 0
-
     for step in steps:
-        cube_len = len(cubes)
+        for cube in cubes:
+            cube.subtract(step)
         if step.on:
-            on_count += step.vol
             cubes.append(step)
-        for i in range(cube_len):
-            shared_volume = step.get_shared_volume(cubes[i])
-            if cubes[i].on and shared_volume:
-                overlap = Cube(False, step.get_shared_coords(cubes[i]))
-                on_count -= shared_volume
-                cubes.append(overlap)
-            elif shared_volume:
-                overlap = Cube(True, step.get_shared_coords(cubes[i]))
-                on_count += shared_volume
-                cubes.append(overlap)
-    return on_count
+    return sum([cube.calculate_volume() for cube in cubes])
 
+p1_ex = part_two(example1)
+print(p1_ex)
 p2_ex = part_two(example2)
-print(p2_ex)
+print(p2_ex)  # 2758447208291213
 assert(p2_ex == 2758514936282235)
 p2 = part_two(steps)
 print(f"Part Two: {p2}")
