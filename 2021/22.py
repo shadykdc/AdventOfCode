0b3247cbfb3fadba367be765d6fa03a6798c5bc8
Katie Shea <katieshea@fb.com>
2021-12-22 01:44:13 -0500

    need to subtract

--- 2021/22.py
+++ 2021/22.py
@@ -10,19 +10,19 @@ class RebootStep:
         self.y2 = int(nums[3])
         self.z1 = int(nums[4])
         self.z2 = int(nums[5])
+        self.vol = self.calculate_volume()
+
+    def get_shared_coords(self, step):
+        return [max(self.x1, step.x1), min(self.x2, step.x2)+1, max(self.y1, step.y1), min(self.y2, step.y2)+1, max(self.z1, step.z1), min(self.z2, step.z2)+1]
 
     def out_of_bounds(self):
-        return abs(self.x1) > 50 or abs(self.x2) > 50
-        or abs(self.y1) > 50 or abs(self.y2) > 50
-        or abs(self.z1) > 50 or abs(self.z2) > 50
+        return abs(self.x1) > 50 or abs(self.x2) > 50 or abs(self.y1) > 50 or abs(self.y2) > 50 or abs(self.z1) > 50 or abs(self.z2) > 50
 
-    def get_volume(self):
+    def calculate_volume(self):
         return abs(self.x1-self.x2) * abs(self.y1-self.y2) * abs(self.z1-self.z2)
 
     def get_shared_volume(self, step):
-        return max(min(self.x2, step.x2))-max(self.x1, step.x1), 0)
-            * max(min(self.y2, step.y2))-max(self.y1, step.y1), 0)
-            * max(min(self.z2, step.z2))-max(self.z1, step.z1), 0)
+        return max(min(self.x2, step.x2)-max(self.x1, step.x1), 0) * max(min(self.y2, step.y2)-max(self.y1, step.y1), 0) * max(min(self.z2, step.z2)-max(self.z1, step.z1), 0)
 
 def get_input(name):
     with open(name, 'r') as f:
@@ -50,23 +50,28 @@ def part_one(steps):
     return len(on_cubes)
 
 assert(part_one(example1) == 590784)
-print(f"Part One: {part_one(steps)}")
-assert(part_one(steps) == 612714)
+p1 = part_one(steps)
+print(f"Part One: {p1}")
+assert(p1 == 612714)
 
 def part_two(steps):
-    on_count
-    for idx, step in enumerate(steps):
+    steps = [step for step in steps]
+    on_count, idx = 0, 1
+    while idx < len(steps):
+        cur = steps[idx]
+        for prev in steps[0:idx]:
+            if cur.on and prev.on and cur.get_shared_volume(prev):
+                overlap = cur.get_shared_coords(prev)
+                steps.insert(idx-1, RebootStep(False, overlap))
+                idx += 1
+        idx += 1
+    for step in steps:
         if step.on:
-            on_count += step.get_volume()
-            for prev in steps[0:idx]:
-                if prev.on:
-                    on_count -= step.get_shared_volume(prev)
-        else:
-            for prev in steps[0:idx]:
-                on_count -= step.get_shared_volume(prev)
+            on_count += step.calculate_volume()
     return on_count
 
 print(part_two(example2))
 assert(part_two(example2) == 2758514936282235)
-print("assert passed...")
-print(f"Part Two: {part_two(steps)}")
+p2 = part_two(steps)
+print(f"Part Two: {p2}")
+assert(p2 > 0)
