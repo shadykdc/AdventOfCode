4ee218d5b0a2a4382773c0d159f73ef5a7906e52
ShadyKDC <kathryn.shea@gmail.com>
2022-12-08 02:21:04 +0000

    day 6

--- 2022/6.py
+++ 2022/6.py
@@ -2,28 +2,34 @@ from pathlib import Path
 from utils import answer
 import re
 
-example = [
-    "1",
-    "2",
-    "3",
-]
+p1_examples = {
+    "bvwbjplbgvbhsrlpgdmjqwftvncz": 5,
+    "nppdvjthqldpwncqszvftbrmjlhg": 6,
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg": 10,
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw": 11,
+}
 
-def get_input(lines):
-    return [int(line.strip()) for line in lines]
+p2_examples = {
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb": 19,
+    "bvwbjplbgvbhsrlpgdmjqwftvncz": 23,
+    "nppdvjthqldpwncqszvftbrmjlhg": 23,
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg": 29,
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw": 26,
+
+}
 
 with open(f'input{Path(__file__).stem}.txt', 'r') as f:
-    puzzle_input = get_input(f.readlines())
+    puzzle_input = f.readlines()[0]
 
-assert(get_input(example) == [1, 2, 3])
+def solution(letters, count=4):
+    idx = count
+    while len(set(letters[idx-count:idx])) != count:
+        idx+=1
+    return idx
 
-def part1(nums):
-    return 1
-
-def part2(nums):
-    return 1
-
-
-answer(part1(get_input(example)), "Example 1")
-answer(part1(puzzle_input), "Part 1")
-answer(part2(get_input(example)), "Example 2")
-answer(part2(puzzle_input), "Part 2")
+for key in p1_examples:
+    answer(solution(key), "Example 1", p1_examples[key])
+answer(solution(puzzle_input), "Part 1", 1175)
+for key in p2_examples:
+    answer(solution(key, 14), "Example 2", p2_examples[key])
+answer(solution(puzzle_input, 14), "Part 2", 3217)
